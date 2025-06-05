a4d1c2a82d941522a710d213c5fd87000c98b741
yogiksuryatmika19 <145512316+yogiksuryatmika19@users.noreply.github.com>
2025-06-05 20:19:23 +0800

    belajar dom

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-// constructor function
-function Player(nama)
-{
-    this.nama = nama;
-    this.energy = 100;
-    this.heal = (amount) =>
-    {
-        this.energy += amount;
-        return `penambahan energy sebesar ${amount}. Total Energy = ${this.heal}`;
-    };
+let judul = document.getElementById("judul");
+judul.style.color = "red";
+judul.style.backgroundColor = "#ccc";
+judul.innerHTML = "Yogi Suryatmika";
+
+const p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
 }
 
-player1 = new Player("player1");
+// class
+const p1 = document.getElementsByClassName("p1");
+p1[0].innerHTML = "ini diubah dari javascript";
+
+// querySelector
+const p4 = document.querySelector("#b p");
+p4.style.color = "green";
+p4.style.fontSize = "30px";
+
+const li2 = document.querySelector(
+  "section#b ul li:nth-child(2)"
+);
+
+li2.style.backgroundColor = "orange";
